3cfeb6b46bbd579937fa08c974c7bc1438262287
Robot7769 <jan.robot7@gmail.com>
2022-04-24 18:09:56 +0200

    nefunguje ;(

--- proj2.c
+++ proj2.c
@@ -25,6 +25,9 @@
 #include "proj2.h"
 #include "funkce.c"
 
+void* oxygen(shmem_t *mem, FILE *output);
+
+void* hydrogen(shmem_t *mem, FILE *output);
   
 int main(int argc, char const *argv[]) {
     if (argc != ARG_COUNT) {
@@ -39,6 +42,12 @@ int main(int argc, char const *argv[]) {
     if (mem == MAP_FAILED) {
         error_exit("Nepodařilo se vytnořit sdílenou paměť\n");
     }
+    
+    sem_init(&mutex ,0 ,1);
+    sem_init(&queue_o ,0 ,0);
+    sem_init(&queue_h ,0 ,0);
+    sem_init(&barier ,0 ,3);
+    sem_init(&print ,0 ,1);
         
     //argument 1 počet kyslíků
     mem->count_o = atoi(argv[1]);
@@ -60,22 +69,23 @@ int main(int argc, char const *argv[]) {
         error_exit("TB není v rozsaho 0<=TB<=1000\n");
     }
 
-    pid_t kyslik = fork();
+    FILE *output = fopen("proj.out","w");
+    if (output == NULL) {
+        munmap(mem, sizeof(shmem_t));
+        error_exit("Nepodařilo se otevřít soubor pro zápis");
+    }
+
+
+    /**
+    pid_t kyslik = 0;
     if (kyslik == 0) {
         //main
-            printf("START\n");
-  
-        pid_t vodik = fork();
-        if (vodik) {
-            //funkce vodik
-            printf("začátek vodíku %d\n", vodik);
-            mem->count_h--;
-            usleep(rand() % 500);
-            printf("konec vodíku %d\n", vodik);
-            exit(0);
+        printf("START\n");
+        for (size_t i = 0; i < 2; i++) {
+            kyslik = fork();
         }
-        usleep(1005);
-        printf("stop\n");
+  
+        
     } else {
         //funkce kyslik
         printf("začátek kyslíku %d\n", kyslik);
@@ -85,9 +95,108 @@ int main(int argc, char const *argv[]) {
         printf("konec kyslíku %d\n", kyslik);
         exit(0);
     }
-    
+    pid_t vodik = fork();
+    if (vodik) {
+        //funkce vodik
+        printf("začátek vodíku %d\n", vodik);
+        mem->count_h--;
+        usleep(rand() % 500);
+        printf("konec vodíku %d\n", vodik);
+        exit(0);
+    }
+    usleep(1005);
+    printf("stop\n");
+    */
+    pthread_t t1,t2, t3;
+    pthread_create(&t1,NULL,hydrogen(mem,output),NULL);
+    pthread_create(&t2,NULL,hydrogen(mem,output),NULL);
+    pthread_create(&t3,NULL,oxygen(mem,output),NULL);
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+
 
     printf("\ndone! %ld %ld %ld %ld\n", mem->count_o, mem->count_h, mem->time_i,mem->time_b);
+    sem_destroy(&mutex);
+    sem_destroy(&queue_o);
+    sem_destroy(&queue_h);
+    sem_destroy(&barier);
+    sem_destroy(&print);
     munmap(mem, sizeof(shmem_t));
     return 0;
+}
+
+
+void print_out(shmem_t *mem, FILE *output,const char *fmt, ...) {
+    sem_wait(&print);
+    mem->id +=1;
+    va_list(argumenty);
+    va_start(argumenty,fmt);
+    fprintf(output, "%ld: ", mem->id);
+    fprintf(stdout, "%ld: ", mem->id); //! test
+    vfprintf(output,fmt,argumenty);
+    vfprintf(stdout,fmt,argumenty); //! test
+    va_end(argumenty);
+    sem_post(&print);
+}
+
+void* oxygen(shmem_t *mem, FILE *output) {
+    sem_wait(&mutex);
+    mem->index_o +=1;       // ID kyslíku
+    print_out(mem, output,"O %d: started\n", mem->index_o);
+    if (mem->time_i != 0) {
+        usleep(rand() % mem->time_i + 1);
+    }
+    mem->out_o +=1;
+    print_out(mem, output,"O %d: going to queue\n", mem->index_o);
+    if (mem->out_h >= 2) {
+        mem->molecule +=1;
+        print_out(mem, output,"O %d: creating molecule %d\n", mem->index_o, mem->molecule);
+        if (mem->time_b != 0) {
+            usleep(rand() % mem->time_b + 1);
+        }
+        sem_post(&queue_h);
+        sem_post(&queue_h);
+        mem->out_h -= 2;
+        sem_post(&queue_o);
+        mem->out_o -= 1;
+        print_out(mem, output,"O %d:molecule %d created\n", mem->index_o, mem->molecule);
+    } else {
+        sem_post(&mutex);
+    }
+    sem_wait(&queue_o);
+    //! bont
+    
+    sem_wait(&barier);
+    sem_post(&mutex);
+}
+
+void* hydrogen(shmem_t *mem, FILE *output) {
+    sem_wait(&mutex);
+    mem->index_h +=1;       // ID vodíku
+    print_out(mem, output,"H %d: started\n", mem->index_h);
+    if (mem->time_i != 0) {
+        usleep(rand() % mem->time_i + 1);
+    }
+    mem->out_h +=1;
+    print_out(mem, output,"H %d: going to queue\n", mem->index_h);
+    if (mem->out_h >= 2 && mem->count_o >= 1) {
+        mem->molecule +=1;
+        print_out(mem, output,"H %d: creating molecule %d\n", mem->index_h, mem->molecule);
+        if (mem->time_b != 0) {
+            usleep(rand() % mem->time_b + 1);
+        }
+        sem_post(&queue_h);
+        sem_post(&queue_h);
+        mem->out_h -= 2;
+        sem_post(&queue_o);
+        mem->out_o -= 1;
+        print_out(mem, output,"H %d:molecule %d created\n", mem->index_h, mem->molecule);
+    } else {
+        sem_post(&mutex);
+    }
+    sem_wait(&queue_h);
+    //! bont
+    
+    sem_wait(&barier);
 }
--- proj2.h
+++ proj2.h
@@ -18,11 +18,24 @@
 
 //! struktura
 typedef struct shmem {
+    size_t id;
     size_t count_o;
     size_t count_h;
     size_t time_i;
     size_t time_b;
+
+    size_t out_o;
+    size_t out_h;
+    size_t index_o;
+    size_t index_h;
+    size_t molecule;
 }shmem_t;
+
+sem_t mutex;
+sem_t queue_o;
+sem_t queue_h;
+sem_t barier;
+sem_t print;
 
 
 #endif // PROJ2_LIB
